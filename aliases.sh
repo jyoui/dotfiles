6d61f9f2a371c8ae87d4a3cbc500e2ec5daa08e2
david feng <davidxifeng@gmail.com>
2012-11-30 17:16:00 +0800

    add aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,16 @@
+# my zsh/bash alias on github
+
+alias c=clear
+alias u='cd ..'
+alias e='exit'
+alias la='ls -a'
+alias v='vim'
+alias mk='make'
+
+# apt
+alias sdas='sudo apt-cache search'
+alias sdai='sudo apt-get install'
+
+#
+alias va='vim ~/david/myvimrc/aliases.sh'
+alias vr='vim ~/.vimrc'
