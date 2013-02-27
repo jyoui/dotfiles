55c5ebce44d582ca43bad9aaea6936c9186eb3e0
david feng <davidxifeng@gmail.com>
2013-02-28 01:30:12 +0800

    整理了一份os x用的命令行别名

---
+++ aliases_for_os_x.sh
@@ -0,0 +1,29 @@
+# my zsh/bash alias on github
+# os x系统版本
+
+alias s=stat
+alias c=clear
+alias u='cd ..'
+alias e='exit'
+alias la='ls -a'
+alias v='vim'
+alias mk='make'
+alias l='less'
+alias g='grep'
+alias st='stat'
+alias du='du -h'
+
+alias pd='pushd'
+alias ppd='popd'
+
+# 常用的目录
+alias ct='cd /tmp'
+
+# 常修改编辑的文件
+alias va='vim ~/aliases.sh'
+alias vr='vim ~/.vimrc'
+
+#常用的工具，命令
+alias cgu='iconv -f GB18030 -t UTF-8'
+alias rst2html='rst2html --no-generator'
+alias vc='vim CMakeLists.txt'
