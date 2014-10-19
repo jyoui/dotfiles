262d0bfe3332a831e3aad679fb05cd4ac3f39c75
david feng <davidxifeng@gmail.com>
2014-10-19 21:44:08 +0800

    update

--- aliases_for_os_x.sh
+++ aliases_for_os_x.sh
@@ -30,23 +30,17 @@ alias sudoedit='sudo -e'
 # 常用的目录
 alias ct='cd /tmp'
 alias cdl='cd ~/david/log'
-alias cw='cd ~/work'
-alias cws='cd ~/work/server/World/src'
-alias cwsp='cd ~/work/server/Prelude/src'
-alias cwc='cd ~/work/client/resources'
-alias cwp='cd ~/work/proto'
+
+alias cw='cd ~/ShortTail'
+alias cws='cd ~/ShortTail/dagger/Resources/src/'
 
 # 常修改编辑的文件
 alias va='vim ~/david/myvimrc/aliases_for_os_x.sh'
 alias vr='vim ~/.vimrc'
 
-#常用的工具，命令
-alias cgu='iconv -f GB18030 -t UTF-8'
-
 alias py=python
 alias bpy=bpython
 alias pym='python -m'
-alias hp='hprotoc -d ../server/World/src '
 
 #Lua
 alias cl='cd ~/Lua/lua-5.1.5/src/'
