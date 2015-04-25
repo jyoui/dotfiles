314893fb7173ba68cdfa00f7ec572ec2e5c00a3d
David Feng <davidxifeng@gmail.com>
2015-04-25 13:03:29 +0800

    update rc

--- zshrc_on_macos.sh
+++ zshrc_on_macos.sh
@@ -1,26 +1,6 @@
 # Path to your oh-my-zsh configuration.
 ZSH=$HOME/.oh-my-zsh
-
 ZSH_THEME="xiong-chiamiov-plus"
-
-# Comment this out to disable bi-weekly auto-update checks
-# DISABLE_AUTO_UPDATE="true"
-
-# Uncomment to change how many often would you like to wait before auto-updates occur? (in days)
-# export UPDATE_ZSH_DAYS=13
-
-# Uncomment following line if you want to disable colors in ls
-# DISABLE_LS_COLORS="true"
-
-# Uncomment following line if you want to disable autosetting terminal title.
-# DISABLE_AUTO_TITLE="true"
-
-# Uncomment following line if you want red dots to be displayed while waiting for completion
-# COMPLETION_WAITING_DOTS="true"
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
 plugins=(git brew cabal sublime)
 
 source $ZSH/oh-my-zsh.sh
@@ -36,12 +16,9 @@ export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin
 #begin of david's custom on Mac
 
 source ~/david/myvimrc/aliases_for_os_x.sh
-
 set -o vi
-
 # ghci 等程序会用到的环境变量
 export EDITOR=vim
-
 export PATH=~/bin:$PATH
 export PATH=~/Library/Haskell/bin:$PATH
 export PATH=/usr/local/openresty/nginx/sbin:$PATH
@@ -57,18 +34,8 @@ export NODE_PATH="/usr/local/lib/node_modules"
 # 需要使用ghc的 -fllvm时启用
 #export PATH=/opt/llvm/bin:$PATH
 
-# cocos2dx android develop
-export ANDROID_SDK_ROOT="/Users/david/software/android-sdk-macosx/"
-export NDK_ROOT="/Users/david/software/android-ndk-r9d/"
-export COCOS_CONSOLE_ROOT=/Users/david/work/cocos2d-x-3.3/tools/cocos2d-console/bin
-export PATH=$COCOS_CONSOLE_ROOT:$PATH
-export ANT_ROOT=/usr/local/bin # for cocos console
-
-export PATH=~/software/android-ndk-r9d:$PATH
-export PATH=~/software/android-sdk-macosx/tools:$PATH
-export PATH=~/software/android-sdk-macosx/platform-tools:$PATH
-
 export PATH=~/.gem/ruby/2.0.0/bin:$PATH
+export PATH=~/.neovim/bin:$PATH
 
 # Add GHC 7.8.4 to the PATH, via http://ghcformacosx.github.io/
 export GHC_DOT_APP="/Applications/ghc-7.8.4.app"
@@ -76,3 +43,15 @@ if [ -d "$GHC_DOT_APP" ]; then
     export PATH="${HOME}/.cabal/bin:${GHC_DOT_APP}/Contents/bin:${PATH}"
 fi
 
+
+# Add environment variable COCOS_CONSOLE_ROOT for cocos2d-x
+export COCOS_CONSOLE_ROOT=/Users/david/work/cocos/quick-3.5/tools/cocos2d-console/bin
+export PATH=$COCOS_CONSOLE_ROOT:$PATH
+
+# Add environment variable QUICK_V3_ROOT for cocos2d-x
+export QUICK_V3_ROOT=/Users/david/work/cocos/quick-3.5
+export PATH=$QUICK_V3_ROOT:$PATH
+
+# Add environment variable COCOS_TEMPLATES_ROOT for cocos2d-x
+export COCOS_TEMPLATES_ROOT=/Users/david/work/cocos/quick-3.5/templates
+export PATH=$COCOS_TEMPLATES_ROOT:$PATH
