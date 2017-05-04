02e5c2f6611f1fd5e68669272715f1aa57ddc560
David Feng <davidxifeng@gmail.com>
2017-05-04 14:48:09 +0800

    clean up

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#需要手动安装的vim插件:
-#创建~/.vim/bundle目录并git clone插件管理器
-#
-#特殊处理的插件:vimproc插件需要make
-    ln -s /home/david/david/myvimrc/vimrc ~/.vimrc
-
-    mkdir -p ~/.config/lilyterm
-    ln -s /home/david/david/myvimrc/lilyterm_default.conf ~/.config/lilyterm/default.conf
