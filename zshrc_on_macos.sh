d8d8ac0f3aee07028d7423e6b158fd19e8a91cad
David Feng <davidxifeng@gmail.com>
2017-09-02 19:49:21 +0800

    new vim setting; use tab and 4 space for red; [ ,ig ] to toggle indent guide

--- zshrc_on_macos.sh
+++ zshrc_on_macos.sh
@@ -31,14 +31,11 @@ export NODE_PATH="/usr/local/lib/node_modules"
 # oracle java 1.8
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 
-
 export PATH=~/Library/Python/2.7/bin:$PATH
-
-#export PATH=~/.gem/ruby/2.0.0/bin:$PATH
+export PATH=~/.gem/ruby/2.0.0/bin:$PATH
 
 #export PATH=~/.neovim/bin:$PATH
 #export PATH=~/openresty/bin:~/openresty/nginx/sbin:$PATH
-#export PATH=~/emacs/bin:$PATH
 #export PATH=/opt/bochs/bin:$PATH
 
 # uncomment when needed
@@ -53,28 +50,4 @@ export VIS_PATH=~/david/vis
 # 使用通过brew安装的gnu 版core utils
 #export PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
 
-function enable_env_for_cocos() {
-
-  export COCOS_CONSOLE_ROOT=/Users/david/bin/cocos2d-x-3.12/tools/cocos2d-console/bin
-  export COCOS_X_ROOT=/Users/david/bin
-  export COCOS_TEMPLATES_ROOT=/Users/david/bin/cocos2d-x-3.12/templates
-  export ANDROID_SDK_ROOT=/Users/david/Library/Android/sdk
-  export NDK_ROOT=/Users/david/bin/android-ndk-r12b
-  export ANT_ROOT=/usr/local/Cellar/ant/1.9.7/bin
-
-  export PATH=~/Library/Android/sdk/tools:$PATH
-  export PATH=~/bin/android-ndk-r12b:$PATH
-  export PATH=$COCOS_CONSOLE_ROOT:$PATH
-  export PATH=$COCOS_X_ROOT:$PATH
-  export PATH=$ANDROID_SDK_ROOT:$PATH
-  export PATH=$COCOS_TEMPLATES_ROOT:$PATH
-  export PATH=$NDK_ROOT:$PATH
-  export PATH=$ANDROID_SDK_ROOT/tools:$ANDROID_SDK_ROOT/platform-tools:$PATH
-  export PATH=$ANT_ROOT:$PATH
-
-}
-
-# quick fix for tmux 2.2 bug on macOS 12
-function tmux() {
-  EVENT_NOKQUEUE=1 /usr/local/bin/tmux
-}
+export HOMEBREW_NO_AUTO_UPDATE=TRUE
